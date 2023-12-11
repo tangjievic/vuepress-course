3d9ff104fe668730176ddaeb955c923afee89cad
tangjievic <981955667@qq.com>
2023-12-11 16:36:38 +0800

    更新vuepress自定义主题课程代码

---
+++ .vuepress/config.ts
@@ -0,0 +1,50 @@
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '../docs/theme/lib/node'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '这是我的个人博客',
+  description: '这是我的第一个 这是我的个人博客 站点',
+  theme:defaultTheme({
+    colorMode:'light',
+    sidebar:{
+        '/docs/':[
+            {
+                text: 'VuePress Reference',
+                link:'',
+            },
+            {
+                text: 'VuePress Reference',
+                collapsible: true,
+                children: [
+                    {
+                        text:'配置',
+                        link:'/reference/cli.md'
+                    }, 
+                    '/reference/config.md'
+                ],
+            },
+            {
+                text: 'Bundlers Reference',
+                collapsible: true,
+                children: ['/reference/bundler/vite.md', '/reference/bundler/webpack.md'],
+            },
+        ]
+    },
+    navbar:[
+        {
+            text: '文档',
+            link: '/docs/',
+            icon:'/desk.png'
+        },
+        {
+            text: '文档x',
+            link: '/docs/',
+            icon:'/desk.png'
+        },
+    ],
+    customFiled:'这是我自定义的一个功能字段',
+    contributors:true,
+    lastUpdated:true
+  })
+})
---
+++ index.md
@@ -0,0 +1,12 @@
+---
+home: true
+title: 首页
+heroText: WETSPACE-TJBLOG
+features:
+  - title: 前端博客
+    details: 工作日常与技术总结，专注于前端领域的技术积累。
+  - title: 有趣好玩
+    details: 强推红警3日冕MOD, 背景故事也很有趣；内容，背景音乐也是yyds；然后再推COD19，有兴趣可以一起来玩哦！
+  - title: 联系方式
+    details: 手机：13265487637; QQ/微信：981955667，欢迎探讨技术问题或者进行项目合作
+---
